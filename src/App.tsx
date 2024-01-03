ecb9d0fdb153c6b7cfd41225a75f3e1fd38b3710
emmanueluwa <emmanuel@fulodev.com>
2024-01-03 14:37:42 +0000

    adding dynamic chart with multiple drop downs

--- src/App.tsx
+++ src/App.tsx
@@ -32,13 +32,15 @@ function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
 
+  const [range, setRange] = useState<number>(30);
+
   //chartjs
   const [data, setData] = useState<ChartData<"line">>();
   const [options, setOptions] = useState<ChartOptions<"line">>({
     responsive: true,
     plugins: {
       legend: {
-        position: "top" as const,
+        display: false,
       },
       title: {
         display: true,
@@ -48,13 +50,64 @@ function App() {
   });
 
   useEffect(() => {
-    const url = "";
-    // "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+    const url =
+      // "";
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
 
     axios.get(url).then((res) => {
       setCryptos(res.data);
     });
   }, []);
+
+  useEffect(() => {
+    if (!selected) return;
+    // make new request and update data state
+    const url =
+      // "";
+      `https://api.coingecko.com/api/v3/coins/${
+        selected?.id
+      }/market_chart?vs_currency=usd&days=${range}&${
+        range === 1 ? `precision=hourly` : `precision=daily`
+      }`;
+    // `https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&precision=daily`;
+
+    axios.get(url).then((res) => {
+      console.log("getting prices!", res.data);
+      setData({
+        labels: res.data.prices.map((price: number[]) => {
+          // /1000 to work with milliseconds
+          return moment
+            .unix(price[0] / 1000)
+            .format(range === 1 ? "HH:MM" : "MM-DD");
+        }),
+        datasets: [
+          {
+            label: "Dataset 1",
+            data: res.data.prices.map((price: number[]) => {
+              return price[1];
+            }),
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+          },
+        ],
+      });
+      setOptions({
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text:
+              `Price of ${selected?.name} over the last ` +
+              range +
+              (range === 1 ? ` day` : ` days`),
+          },
+        },
+      });
+    });
+  }, [selected, range]);
 
   return (
     <>
@@ -62,28 +115,6 @@ function App() {
         onChange={(e) => {
           const c = cryptos?.find((x) => x.id === e.target.value);
           setSelected(c);
-          // make new request and update data state
-          const url = "";
-          // `https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&precision=daily`;
-          axios.get(url).then((res) => {
-            console.log("getting prices!", res.data);
-            setData({
-              labels: res.data.prices.map((price: number[]) => {
-                // /1000 to work with milliseconds
-                return moment.unix(price[0] / 1000).format("MM-DD");
-              }),
-              datasets: [
-                {
-                  label: "Dataset 1",
-                  data: res.data.prices.map((price: number[]) => {
-                    return price[1];
-                  }),
-                  borderColor: "rgb(255, 99, 132)",
-                  backgroundColor: "rgba(255, 99, 132, 0.5)",
-                },
-              ],
-            });
-          });
         }}
         defaultValue="default"
       >
@@ -97,6 +128,15 @@ function App() {
             })
           : null}
         <option value="default">Choose an option</option>
+      </select>
+      <select
+        onChange={(e) => {
+          setRange(parseInt(e.target.value));
+        }}
+      >
+        <option value={30}>30days</option>
+        <option value={7}>7days</option>
+        <option value={1}>1day</option>
       </select>
       {selected ? <CryptoSummary crypto={selected} /> : null}
       {data ? (
